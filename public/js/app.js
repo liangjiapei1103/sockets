5c798bcdf5c7f8a0adb41f5f79c2b5e9a2612f55
Jiapei Liang <liang98@purdue.edu>
2016-01-29 21:32:41 -0500

    Add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,8 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
 	console.log('New message:');
 	console.log(message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>')
 });
 
 // Handle submitting of new message
@@ -22,4 +24,6 @@ $form.on('submit', function (event) {
 	});
 
 	$message.val('');
+
+
 });
